18b320e353d399afb9e0d3fe461018895721a1ae
Vsevolod <liberov.vsevolod@gmail.com>
2020-10-08 21:54:36 +0300

    make correct query with ingredients

--- pizzaService/handlers/pizzaList.ts
+++ pizzaService/handlers/pizzaList.ts
@@ -2,7 +2,16 @@ import { makeMongoClient } from '../mongoClient';
 
 export async function pizzaListHandler() {
   const { close, db } = await makeMongoClient();
-  const items = await db.collection('pizzas').find();
+  const items = await db.collection('pizzas').aggregate([
+    {
+      $lookup: {
+        from: 'ingredients',
+        localField: 'ingredients',
+        foreignField: '_id',
+        as: 'ingredients',
+      },
+    },
+  ]);
   const arr = await items.toArray();
   close();
   return arr;
